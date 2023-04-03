c5d6c2d1ca187d1e84a70ab4a0e2b60891603972
AbdCsko <129372380+AbdCsko@users.noreply.github.com>
2023-04-03 10:09:26 +0200

    insta clone

---
+++ instagram-app/instagram-app/ContentView.swift
@@ -0,0 +1,195 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 0.0) {
+            Header()
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                Stories()
+                
+                Divider()
+                
+                Post()
+                
+                Post(image: "icybay", description: "my second travel")
+            }
+            
+            VStack(spacing: 0.0) {
+                Divider()
+                
+                HStack {
+                    Image("home")
+                    Spacer()
+                    Image("search")
+                    Spacer()
+                    Image("reels")
+                    Spacer()
+                    Image("shop")
+                    Spacer()
+//                    Image("twinlake")
+//                        .resizable()
+//                        .frame(width: 21, height: 21)
+//                        .cornerRadius(50)
+            }
+                .padding(.horizontal, 25)
+                .padding(.top, 10)
+                
+            }
+    
+        }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+struct Header: View {
+    var body: some View {
+        HStack {
+            Image("logo")
+            
+            Spacer()
+            
+            HStack(spacing: 20.0) {
+                Image("Icons")
+            }
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 3)
+    }
+}
+
+struct Story: View {
+    var image: String = "twinlake"
+    var name: String = "Willie Yam"
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Image(image)
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .cornerRadius(50)
+            }
+            .overlay(
+                Circle()
+                    .stroke(LinearGradient(colors: [.red, .purple, .red, .orange, .yellow, .orange]  , startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 2.3)
+                    .frame(width: 68, height: 68)
+            )
+            .frame(width: 70, height: 70)
+            
+            Text(name)
+                .font(.caption)
+        }
+    }
+}
+
+struct Stories: View {
+    var body: some View {
+        ScrollView(.horizontal,  showsIndicators: false) {
+            HStack(spacing: 15.0) {
+                Story()
+                Story(image: "hiddenlake" , name: "Meng To")
+                Story(image: "icybay" , name: "Akson")
+                Story(image: "charleyrivers" , name: "Steph")
+                Story(image: "chilkoottrail" , name: "Sam")
+                Story(image: "chincoteague" , name: "Dara")
+                Story(image: "lakemcdonald" , name: "Sourany")
+                Story(image: "rainbowlake" , name: "Pom")
+            }
+            .padding(.horizontal, 8)
+            
+        }
+        .padding(.vertical , 10)
+    }
+}
+
+struct Postheader: View {
+    var body: some View {
+        HStack {
+            HStack {
+                Image("twinlake")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .cornerRadius(50)
+                
+                Text("Willie Yam")
+                    .font(.caption)
+                    .fontWeight(.bold)
+            }
+            Spacer()
+            Image("more")
+        }
+        .padding(.vertical, 10)
+        .padding(.horizontal, 8)
+    }
+}
+
+struct PostContent: View {
+    var image = ""
+    var body: some View {
+        VStack() {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack {
+                HStack() {
+                    Image("heart")
+                }
+                
+           Spacer()
+                Image("bookmark")
+            }
+//            .padding(.horizontal, 12)
+//            .padding(.vertical, 9)
+        }
+    }
+}
+
+struct Post: View {
+    var image: String = "twinlake"
+    var description: String = "I miss traveling to Canada"
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            Postheader()
+            
+            PostContent(image: image)
+            
+            Text("Liked by Meng To and others")
+                .font(.footnote)
+                .padding(.horizontal, 12)
+            
+            Text(description)
+                .font(.footnote)
+                .padding(.horizontal, 12)
+            
+            HStack {
+                HStack(spacing: 7.0) {
+                    Image("twinlake")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .cornerRadius(50)
+                    
+                    Text("Add comment...")
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("😍")
+                    Text("😂")
+                    Image(systemName: "plus.circle")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding(.horizontal, 12)
+            .padding(.vertical, 9)
+        }
+    }
+}
---
+++ instagram-app/instagram-app/instagram_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_appApp.swift
+//  instagram-app
+//
+//  Created by cissokho abdoulaye on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct instagram_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
